06768bd0f314b5d9d5406f9d6bbfabc7d7190a98
Shariq Choudhary <khanshariq92213@gmail.com>
2025-05-19 23:45:47 +0530

    Minor change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["media.geeksforgeeks.org"],
+    domains: ["media.geeksforgeeks.org","simpleprogrammer.com"],
   },
 };
 
